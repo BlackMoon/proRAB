c1f40ba89970ccaae73827b1f001981b1477cf29
Romeo <rrv.kazan@gmail.com>
2020-05-06 13:39:57 +0300

    reengineering

---
+++ Engine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prorab
+{
+    public class Engine
+    {
+        public object Run(string body)
+        {
+            return null;
+        }
+    }
+}
